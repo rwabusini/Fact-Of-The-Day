e8cfe07ab6cfe322b9a061cf4e445dffbf9292cf
sawsan89 <sawsan.h.m.89@gmail.com>
2020-05-07 09:06:23 +0300

    button_function

---
+++ welcome_java.js
@@ -0,0 +1,8 @@
+document.getElementById("animal").onclick = function () {
+        location.href = "animal.html";
+    };
+
+
+document.getElementById("random").onclick = function () {
+        location.href = "Random.html";
+    };
